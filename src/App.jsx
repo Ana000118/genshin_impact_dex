ae1f1895ce8cb57936e5161b11e0f0aa2d9d10a6
Ana Karina Rojas Juan <rojasjuananakarina@gmail.com>
2022-05-26 10:09:22 -0500

    inserción de la función para select principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,60 @@
-import './App.css';
+import { useState } from "react";
+
+const tipos ={
+  artifacs:"artefactos",
+  boos:"jefes",
+  Characters:"personajes",
+  consumables:"consumibles",
+  domains:"dominios",
+  elements:"elementos",
+  enemies:"enemies",
+  materials:"materiales",
+  natios:"naciones",
+  weapons:"armas",
+};
 
 function App() {
+  
+const [genshinState, setGenshinState] = useState({
+  types: []
+});
+
+const fetchGenshinApi = async (item, url = "https://api.genshin.dev/") => {
+  const respJson = await fetch(url);
+  if (item === "types"){
+    setGenshinState({
+      types: respJson.types,
+    });
+  }
+  else{
+    setGenshinState({
+      [item]: respJson,
+    });
+  }
+};
+
+fetchGenshinApi ("types");
+
+const handleChangeType =({target}) => {
+  const url = `https://api.genshin.dev/${target.value}`; 
+  fetchGenshinApi(target.value,url);
+  console.log(genshinState);
+
+};
+  
+
   return (
-    <div className="App">
-      <h3>prueba</h3>
+    <div className="App container">
+      <h3>Genshin Impact Dex</h3>
+      <hr/>
+      <select name="types" onChange={handleChangeType}>
+        <option value="">Seleccione un elemento</option>
+        {genshinState.types.map((type) => (
+          <option key={type} value={type} >
+            {tipos[type]}
+            </option>
+        ))}
+      </select>
  
     </div>
   );
