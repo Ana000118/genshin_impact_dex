35c0f2c84a8a47d5044751cf061b681f177fc851
Ana Karina Rojas Juan <rojasjuananakarina@gmail.com>
2022-06-13 10:03:35 -0500

    inserción del submenú artefactos

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,77 @@
-import { useState } from "react";
-
-const tipos ={
-  artifacts:"Artefactos",
-  boss:"Jefes",
-  characters:"Personajes",
-  consumables:"Consumibles",
-  domains:"Dominios",
-  elements:"Elementos",
-  enemies:"Enemigos",
-  materials:"Materiales",
-  nations:"Naciones",
-  weapons:"Armas",
-};
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  
-const [genshinState, setGenshinState] = useState({
-  types: [],
-});
+  const [genshinState, setGenshinState] = useState({
+    types: []
+  });
 
-const fetchGenshinApi = async (item, url = "https://api.genshin.dev/") => {
-  const respuesta = await fetch(url);
-  const respJson = await respuesta.
-  json();
-  if (item === "types"){
-    setGenshinState({
-      ...genshinState,
-      types: respJson.types,
-    });
-  }
-  else{
-    setGenshinState({
-      ...genshinState,
+  const tipos ={
+    artifacts:"Artefactos",
+    boss:"Jefes",
+    characters:"Personajes",
+    consumables:"Consumibles",
+    domains:"Dominios",
+    elements:"Elementos",
+    enemies:"Enemigos",
+    materials:"Materiales",
+    nations:"Naciones",
+    weapons:"Armas",
+  };
+
+  const fetchGenshinApi = async(item, url = "https://api.genshin.dev/")=>
+  {
+    const respuesta = await fetch(url);
+    const respJson = await respuesta.json();
+    if (item === "types"){
+      setGenshinState({
+        ...genshinState,
+        types: respJson.types,
+      });
+    }else{
+      setGenshinState({
+        ...genshinState,
       [item]: respJson,
-    });
-  }
-};
+      });
+    }
+    console.log(respJson);
+  };
 
-fetchGenshinApi ("types");
+  useEffect(() => {
+    fetchGenshinApi("types");
+  }, []);
 
-const handleChangeType = ({target}) => {
-  const url = `https://api.genshin.dev/${target.value}`; 
-  fetchGenshinApi(target.value,url);
-  console.log(genshinState);
 
-};
+  const handleChangeType = ({target}) => {
+    const url = `https://api.genshin.dev/${target.value}`
+    fetchGenshinApi(target.value, url);
+  };
 
   return (
-    <div className="App container">
-      <h3>Genshin Impact Dex</h3>
-      <hr/>
-      <select name="types" onChange={handleChangeType}>
-        <option value="">Seleccione un elemento</option>
-        {genshinState.types.map((type) => (
-          <option key={type} value={type} >
-            {tipos[type]}
-            </option>
+    <div className='App container'>
+    <h1 style={{color: "white"}}>Genshin Impact Dex</h1>
+    <hr />
+    <select name="types" onChange={handleChangeType}>
+    <option value="">Seleccione un elemento</option>
+      {genshinState.types.map((type) => (
+      <option key={type} value={type}>
+        {tipos[type]}
+      </option>
+      ))}
+    </select>
+    {
+      genshinState.artifacts && <select name="artifacts">
+        <option value="">Seleccione un set de artefactos</option>
+        {genshinState.artifacts.map((artifact) => (
+          <option key={artifact} value={artifact}>
+            {artifact}
+          </option>
         ))}
       </select>
+    }
     </div>
   );
 }
 
 export default App;
+
